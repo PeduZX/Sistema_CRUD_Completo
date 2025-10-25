cdd5b3148a4954ffce0677c70ad0b17a98689070
Pedro <107699339+PeduZX@users.noreply.github.com>
2025-10-25 17:35:34 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const carImg = document.getElementById("car-img");
+const modelo = document.getElementById("modelo");
+const marca = document.getElementById("marca");
+const origem = document.getElementById("origem");
+const carroceria = document.getElementById("carroceria");
+const inputFile = document.getElementById("input-file");
