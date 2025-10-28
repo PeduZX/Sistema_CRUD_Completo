f45ddae4f11a1500649de8b575876291becb896f
Pedro <107699339+PeduZX@users.noreply.github.com>
2025-10-28 11:21:56 -0300

    Add files via upload

--- js/script-cadastro.js
+++ js/script-cadastro.js
@@ -30,7 +30,6 @@ async function cadastrar() {
 
 
 async function login() {
-
   const email = document.getElementById('email').value.trim();
   const senha = document.getElementById('senha').value.trim();
   const msg = document.getElementById('msg');
@@ -43,10 +42,26 @@ async function login() {
     });
 
     const data = await response.json();
-      if (response.ok) {
-      msg.textContent = data.message;
+
+    if (response.ok && email === "Admin@teste.com" && senha === "1234") { 
+      // Exemplo: armazenar email e id no sessionStorage
+      const usuario = {
+        email: email,
+       
+        nome: data.nome || "Administrador"
+      };
+
+      sessionStorage.setItem("usuarioLogado", JSON.stringify(usuario));
+
+      msg.textContent = data.message || "Login bem-sucedido!";
       msg.style.color = 'green';
       document.querySelector('form').reset();
+
+      // exemplo: redirecionar para outra página
+      setTimeout(() => {
+        window.location.href = "dashboard.html";
+      }, 5000)
+
     } else {
       msg.textContent = data.error || 'Erro ao Logar usuário.';
       msg.style.color = 'red';
