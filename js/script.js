48ee28ecaddc80945de36a02369e0dc8465987c5
Pedro <107699339+PeduZX@users.noreply.github.com>
2025-10-29 00:41:39 -0300

    Delete js/script.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const carImg = document.getElementById("car-img");
-const modelo = document.getElementById("modelo");
-const marca = document.getElementById("marca");
-const origem = document.getElementById("origem");
-const carroceria = document.getElementById("carroceria");
-const inputFile = document.getElementById("input-file");
